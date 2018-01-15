e33c98bcd09c558d724f96c112c7b7b42839436a
falramahi <faisal.ramahi@gmail.com>
2018-01-15 09:47:28 -0800

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
+  host {'web.pupep.vm':
+    ensure => present,
+    ip     => '172.18.0.3',
+  }
+  host {'db.pupep.vm':
+    ensure => present,
+    ip     => '172.18.0.3',
+  }
 }
