b2b16d6349c80994e6e25090cf7376d12e5ed82b
falramahi <faisal.ramahi@gmail.com>
2018-01-15 11:13:09 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -15,7 +15,7 @@ class minecraft {
   }
   files {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///module/minecraft/minecraft.service',
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service {'minecraft':
     ensure => running,
